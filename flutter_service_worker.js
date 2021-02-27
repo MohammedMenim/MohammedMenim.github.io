e3fdd85217ba1155c3c10675d6f1cfecde7e3406
Mohammed Menim <62985997+MohammedMenim@users.noreply.github.com>
2021-02-27 16:58:37 +0100

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "96583e769631bb6658a1304ba0483bb1",
+"assets/assets/data/stops.txt": "23af4a604d7297b6e4c97ea3f90dce3d",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/NOTICES": "4c9471579ca37cd2316b183c8ebf3d9e",
@@ -11,9 +12,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ae4abd4522615c6d23ba1dc05d520a04",
-"/": "ae4abd4522615c6d23ba1dc05d520a04",
-"main.dart.js": "768399a8b063180e27443052ac33d71d",
+"index.html": "fcd4c5fcc3f08b8220c588638f3c4df9",
+"/": "fcd4c5fcc3f08b8220c588638f3c4df9",
+"main.dart.js": "cb687b01e33af04138353527c96a777a",
 "manifest.json": "c5892224b8638f883387765acbd1da04",
 "version.json": "7d01db3330c0f81fcf135a6d0a07b1f7"
 };
