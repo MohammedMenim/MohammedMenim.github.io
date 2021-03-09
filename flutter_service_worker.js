e7d20a88c909270814b2184d061dec481bcb507f
Mohammed Menim <62985997+MohammedMenim@users.noreply.github.com>
2021-03-09 02:16:02 +0100

    d

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3a849306566a9600021f59adef773e04",
-"/": "3a849306566a9600021f59adef773e04",
-"main.dart.js": "7a7a75d57ace92282fb0251b89f4d6af",
+"index.html": "492f8891aee09a38338f57f6f7308390",
+"/": "492f8891aee09a38338f57f6f7308390",
+"main.dart.js": "e456ee2695dd243f2905b91de02c5ca0",
 "manifest.json": "c5892224b8638f883387765acbd1da04",
 "version.json": "7d01db3330c0f81fcf135a6d0a07b1f7"
 };
